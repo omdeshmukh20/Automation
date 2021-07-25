2e04a80a6aa8768ddc16ddd887b53986f021e821
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-25 14:30:12 +0530

    Giving arguements

---
+++ Script4.py
@@ -0,0 +1,46 @@
+#Discription: Giving arguements
+#Date: 25/07/21
+#Author : Om Deshmukh
+
+import os
+import time
+import psutil
+from sys import *
+
+def ProcessDisplay(FolderName="Marvellous"):
+    
+    Data = []
+
+    if not os.path.exists(FolderName):
+    	os.mkdir(FolderName)
+
+    return
+        
+    for proc in psutil.process_iter():
+        value = proc.as_dict(attrs = ['pid','name','username'])
+        Data.append(value)
+        
+    return Data
+    
+def main():
+    print("------ Marvellous Infosystems ------")
+    print("Script title : "+argv[0])
+
+    if(len(argv) !=2):
+    	print("Insufficient arguement")
+    	exit()
+
+    if(argv[1]=="-u"):
+    	print("Usage:Application_Name Directory_NAme")
+    	exit()
+
+    if(argv[1]=="-h"):
+    	print("Help: It is used to create log file of running process")
+    	exit()	
+
+    arr = ProcessDisplay()
+    for element in arr:
+        print(element)
+        
+if __name__ == "__main__":
+    main()
