047a66c0a778512d7e4b0eb3dd98cca8ffc305ca
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-25 14:28:26 +0530

    Taking list of ddemo processes in our lap

---
+++ Script3.py
@@ -0,0 +1,28 @@
+#Discription: Taking list of ddemo processes in our lap
+#Date: 25/07/21
+#Author : Om Deshmukh
+
+import psutil
+from sys import *
+
+def ProcessDisplay():
+    print("List of running processess")
+    
+    Data = []
+    
+    for proc in psutil.process_iter():
+        value = proc.as_dict(attrs = ['pid','name','username'])
+        Data.append(value)
+        
+    return Data
+    
+def main():
+    print("------ Marvellous Infosystems ------")
+    print("Script title : "+argv[0])
+
+    arr = ProcessDisplay()
+    for element in arr:
+        print(element)
+        
+if __name__ == "__main__":
+    main()
